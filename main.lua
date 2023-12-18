dead18666c8bb3e1960ee58f7b8e82f2d2386406
Mike Mehl <michaelalanmehl@gmail.com>
2023-12-17 22:21:59 -0500

    batteries

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+---@class Object
+local Object = {}
+
+---@return Object
+function Object:new()
+	local object = {}
+	setmetatable(object, self)
+	self.__index = self
+	return object
+end
+
 function love.draw()
 	love.graphics.print("Hello World", 400, 300)
 end
